46f90e6cdb49b755d6ce5985c48f1bc5e23a7f68
Steven <stevenjesusespinal@gmail.com>
2020-06-27 14:48:32 -0400

    added about window and opened link in default browser

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-const { app, BrowserWindow, Menu, globalShortcut } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
 process.env.NODE_ENV = "development";
 
 const isDev = process.env.NODE_ENV !== "production" ? true : false;
 const isMac = process.platform === "darwin" ? true : false;
-const isWin = process.platform === "win32" ? true : false;
+// const isWin = process.platform === "win32" ? true : false;
 
 let mainWindow;
+let aboutWindow;
 
 function createMainWindow() {
   mainWindow = new BrowserWindow({
@@ -19,6 +20,24 @@ function createMainWindow() {
 
   // mainWindow.loadURL(`file://${__dirname}/app/index.html`);
   mainWindow.loadFile(`./app/index.html`);
+}
+
+function createAboutWindow() {
+  aboutWindow = new BrowserWindow({
+    width: 300,
+    height: 300,
+    title: "About Image Shrink",
+    icon: `${__dirname}/assets/Icon_256x256.png`,
+    resizable: false,
+  });
+
+  aboutWindow.webContents.on("new-window", function (e, url) {
+    e.preventDefault();
+    require("electron").shell.openExternal(url);
+  });
+
+  // mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+  aboutWindow.loadFile(`./app/about.html`);
 }
 
 app.on("window-all-closed", () => {
@@ -39,10 +58,10 @@ app.on("ready", () => {
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu);
 
-  globalShortcut.register("CmdOrCtrl+R", () => mainWindow.reload());
-  globalShortcut.register(isMac ? "Cmd+Alt+I" : "Ctrl+Shift+I", () =>
-    mainWindow.toggleDevTools()
-  );
+  // globalShortcut.register("CmdOrCtrl+R", () => mainWindow.reload());
+  // globalShortcut.register(isMac ? "Cmd+Alt+I" : "Ctrl+Shift+I", () =>
+  //   mainWindow.toggleDevTools()
+  // );
 
   mainWindow.on("closed", () => (mainWindow = null));
 });
@@ -51,13 +70,32 @@ const menu = [
   ...(isMac
     ? [
         {
-          role: "appMenu",
+          label: app.name,
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
         },
       ]
     : []),
   {
     role: "fileMenu",
   },
+  ...(!isMac
+    ? [
+        {
+          label: "Help",
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
   ...(isDev
     ? [
         {
